2e7216c1908330283669c029ba8df62b6c65bc72
MetaWebDev <metawebdevsolutions@gmail.com>
2025-12-11 21:32:48 -0600

    feat: Agregar animaciones de transición entre páginas con framer-motion

---
+++ src/app/template.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      initial={{ x: 50, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: -50, opacity: 0 }}
+      transition={{ ease: 'easeInOut', duration: 0.5 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
